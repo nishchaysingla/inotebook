8c8943a4ab203a617b1706d200a17cd8e2e9bd86
Pujan <pujanshah825@gmail.com>
2024-06-03 04:13:18 +0530

    good going till 62

--- src/App.js
+++ src/App.js
@@ -1,17 +1,3 @@
-
-// import './App.css';
-// import Navbar from './components/Navbar';
-// import Home from './components/Home';
-// function App() {
-//   return (
-//     <div>
-//        <Navbar/>
-//       <Home/>
-//     </div>
-//   );
-// }
-
-// export default App;
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/Home';
