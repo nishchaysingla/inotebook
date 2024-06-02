b5000c25266ae9df42136725dc4e5814600f651e
Pujan <pujanshah825@gmail.com>
2024-06-03 04:13:45 +0530

    good going till 62

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import About from './components/About';
 import {
   BrowserRouter as Router,
   Route,
-  Routes,
-
+  Routes
 } from "react-router-dom";
+import NoteState from './context/notes/NoteState';
+
 function App() {
   return (
       <>
+      <NoteState>
       <Router>
           <Navbar />
-          {/* <h1> what is this? </h1> */}
+      
+        
             <Routes>
             <Route exact path="/" element={<Home/>} />
             <Route exact path="/about" element={<About/>} />
@@ -21,10 +24,10 @@ function App() {
             </Routes>
           
         </Router>
-      
+        </NoteState>
       </>
       
-    
+    // eslint-disable-next-line
   );
 }
 
